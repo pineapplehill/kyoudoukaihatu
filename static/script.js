2f808c36828c0393517f4ecc419dbf5b1d561b5a
ddd128 <145101792+ddd128@users.noreply.github.com>
2024-07-02 19:18:33 +0900

    メッセージ未入力状態で送信ボタンが押せないようにしました。

--- static/script.js
+++ static/script.js
@@ -1,7 +1,18 @@
 const messageInput = document.getElementById("message-input");
+const submitButton = document.getElementById("submit-button")
 
 setInterval(() => {
   if (messageInput.value === "") {
     window.location.reload();
   }
 }, 5 * 1000);
+
+submitButton.disabled = true;
+setInterval(() => {
+  if (messageInput.value === "") {
+    submitButton.disabled = true;
+  }
+  else {
+    submitButton.disabled = false;
+  }
+}, 10);
